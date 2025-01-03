8d33b00e7cd624c3cfb44931ed15a6ef8b6deb38
David Bonser <david@codapt.co.uk>
2025-01-03 16:22:56 +0000

    views part one complete

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
+    objects = None # todo remove this line
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -17,3 +18,18 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+def __str__(self):
+    return f"Comment {self.body} by {self.author}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
