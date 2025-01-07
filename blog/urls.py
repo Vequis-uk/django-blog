9c37a4395410162a38c5684b52cf61bdc7a3719c
David Bonser <david@codapt.co.uk>
2025-01-07 16:20:20 +0000

    views 2 started

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from . import views
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
-    path('summernote/', include('django_summernote.urls')),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
